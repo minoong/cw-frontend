3cd39c8d279ac6d9a6a9f3be293f63139f76e37d
lmw <alsdndjdrktl@gmail.com>
2023-07-01 10:25:36 +0900

    feat: react storybook

---
+++ .integrated_storybook/.storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import fs from 'fs'
+
+function getStories({ dir = 'ui' }) {
+ const dirName = `packages/${dir}`
+ const scope = fs.readdirSync(dirName)
+
+ return scope
+  .map((pkg) => `${dirName}/${pkg}/stories`)
+  .filter((storyDir) => fs.existsSync(storyDir))
+  .map((storyDir) => `../../${storyDir}/*.stories.@(js|jsx|ts|tsx)`)
+}
+
+const config: StorybookConfig = {
+  stories: [
+    ...getStories({ dir: 'ui' }),
+   ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
--- packages/ui/react-ui/stories/Button.stories.ts
+++ packages/ui/react-ui/stories/Button.stories.ts
@@ -1,44 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '../src';
 
-import { Button } from '../src'
-
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
- title: 'Example3/Button2',
- component: Button,
- tags: ['autodocs'],
- argTypes: {
-  backgroundColor: { control: 'color' },
- },
-} satisfies Meta<typeof Button>
+  title: 'Example3/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof Button>;
 
-export default meta
-type Story = StoryObj<typeof meta>
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
- args: {
-  primary: true,
-  label: 'Button',
- },
-}
-
-export const Secondary: Story = {
- args: {
-  label: 'Button',
- },
-}
-
-export const Large: Story = {
- args: {
-  size: 'large',
-  label: 'Button',
- },
-}
-
-export const Small: Story = {
- args: {
-  size: 'small',
-  label: 'Button',
- },
-}
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
